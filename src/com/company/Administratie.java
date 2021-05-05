805eaba5c4d51fd93d6833161ca5549de84adf78
sneakwish <wishal_wishnu@hotmail.com>
2021-05-05 16:38:41 +0200

    Class leden, class administratie begin

---
+++ src/com/company/Administratie.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Administratie {
+    private ArrayList<Leden> ledenLijst = new ArrayList<Leden>();
+
+
+    public String hoi() {
+        return "hoi";
+    }
+
+
+}
---
+++ src/com/company/AdministratieTest.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdministratieTest {
+
+    @Test
+    void hoiTest() {
+        Administratie yo = new Administratie();
+        assertEquals("hoi", yo.hoi());
+    }
+
+    @Test
+    void
+}
