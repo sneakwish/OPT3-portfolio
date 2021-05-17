2e7a5fb941c785a526e8bb0c840df16d2569cc97
sneakwish <wishal_wishnu@hotmail.com>
2021-05-17 20:59:29 +0200

    administratie junit + aanwezigheid

--- src/capoeira1/Administratie.java
+++ src/capoeira1/Administratie.java
@@ -4,11 +4,46 @@ import java.util.ArrayList;
 
 public class Administratie {
     private ArrayList<Lid> lidLijst = new ArrayList<Lid>();
+    private Boolean heeftGetraind;
+    private Boolean betaald;
 
 
-    public String hoi() {
-        return "hoi";
+    public int melding(Lid lid){
+        if(heeftGetraind && lid.getIsLid() && !betaald){
+            System.out.println("jan heeft getraind en niet betaald: Niet OK!");
+            return 1;
+        }
+        if(heeftGetraind && lid.getIsLid() && betaald){
+            System.out.println("jan heeft getraind en betaald: OK!");
+            return 2;
+        }
+        else {
+            System.out.println("jan is niet lid dus hoeft (nog) niet te betalen");
+            return 0;
+        }
     }
 
+    public ArrayList<Lid> getLidLijst() {
+        return lidLijst;
+    }
 
+    public void setLidLijst(ArrayList<Lid> lidLijst) {
+        this.lidLijst = lidLijst;
+    }
+
+    public Boolean getHeeftGetraind() {
+        return heeftGetraind;
+    }
+
+    public void setHeeftGetraind(Boolean heeftGetraind) {
+        this.heeftGetraind = heeftGetraind;
+    }
+
+    public Boolean getBetaald() {
+        return betaald;
+    }
+
+    public void setBetaald(Boolean betaald) {
+        this.betaald = betaald;
+    }
 }
